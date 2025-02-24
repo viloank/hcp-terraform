7994babe31af07d0a7118e121a76d473bbf721e4
viloank <49016723+viloank@users.noreply.github.com>
2025-02-24 09:46:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform { 
+  cloud { 
+    
+    organization = "ankit-org1" 
+
+    workspaces { 
+      name = "hcp-terraform" 
+    } 
+  } 
+}
+
+provider "aws" {
+    region = "ap-south-1"
+    
+}
+
+resource "aws_instance" "myhost" {
+    ami = "ami-0d682f26195e9ec0f"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "hcp-host2"
+    }
+}
